8e426b17ac543df311c7124a9e77f622b4627141
mcmasterc <113039306+mcmasterc@users.noreply.github.com>
2023-05-20 14:51:08 -0400

    added functions from XeApp code to load ventilation data files
    I just took the LoadReadData functions and removed the pieces that were not needed for this project (e.g. loading diffusion/gas exchange data or loading DICOM Philips images). The function is more concise now and thus easier to debug. Loading data into the workspace where it can be processed is a crucial starting point for the code.

---
+++ +Functions/LoadReadData_VentDataList.m
@@ -0,0 +1,79 @@
+
+function [Ventilation] = LoadReadData_VentDataList(MainInput)
+% Initialize output variables
+Ventilation.Image = [];
+Ventilation.filename = [];
+Ventilation.folder = [];
+
+
+%% Load/Read Xenon data 
+
+if strcmp(MainInput.XeDataext,'.mat') == 1 
+%         DataFiles = dir([MainInput.XeDataLocation,'\*.mat']);
+        file_name = MainInput.XeFileName;
+        file_folder = MainInput.XeDataLocation;
+%         file_with_path = strcat(file_folder,'\',file_name);  % join path and filename to open
+        load(MainInput.XeFullPath);
+        file_name2 = file_name; 
+        file_name2(end-3:end)=[];                    
+        Ventilation.Image = eval(file_name2);
+        Ventilation.filename = file_name;
+        Ventilation.folder = file_folder;       
+   
+elseif strcmp(MainInput.XeDataext,'.nii') == 1 || strcmp(MainInput.XeDataext,'.gz') == 1 
+%         DataFiles = MainInput.XeDataLocation; 
+%         if length(DataFiles) == 1
+%             DataFiles = dir([MainInput.XeDataLocation,'\*.nii.gz']); 
+%         else 
+%            DataFiles = dir([MainInput.XeDataLocation,'\*.nii']); 
+%         end     
+        file_name = MainInput.XeFileName;
+        file_folder = MainInput.XeDataLocation;
+        A1 = LoadData.load_nii(MainInput.XeFullPath); % Original
+        A=A1.img;
+        if strcmp(MainInput.ImgOrientation,'Coronal') == 1
+            gzfile_coronal = [3 1 2];
+            A=permute(A,gzfile_coronal);
+            A=rot90(A,2); 
+        else
+            A=imrotate(A,90);
+            A=flip(A,2);
+        end  
+        A = double(squeeze(A));                     
+        Ventilation.Image = A;
+        Ventilation.filename = file_name;
+        Ventilation.folder = file_folder;       
+    
+elseif strcmp(MainInput.XeDataext,'.data') == 1  
+    if strcmp(MainInput.ScannerSoftware, '5.3.1') == 1 ...
+            || strcmp(MainInput.ScannerSoftware, '5.6.1') == 1
+        [Image, file_folder, file_name] = ...
+            LoadData.LoadData_Gas_VentDiff_Philips_GRE(MainInput.XeDataLocation);
+        Ventilation.Image = Image;
+        Ventilation.filename = file_name;
+        Ventilation.folder = file_folder;     
+
+    elseif strcmp(MainInput.ScannerSoftware, '5.9.0') == 1                
+        [Image, file_folder, file_name] = ...
+            LoadData.LoadData_Gas_VentDiff_Philips_GRE_R590(MainInput.XeDataLocation);
+        Ventilation.Image = Image;
+        Ventilation.filename = file_name;
+        Ventilation.folder = file_folder;          
+                
+    end
+%--------------------- add new read load function here --------------------
+% elseif strcmp(MainInput.XeDataType,'add DataType') == 1
+%     if strcmp(MainInput.AnalysisType,'Ventilation') == 1                 
+%       %  add load/read function here 
+%     elseif strcmp(MainInput.AnalysisType,'Diffusion') == 1 
+%       %  add load/read function here
+%     elseif strcmp(MainInput.AnalysisType,'GasExchange') == 1 
+%       %  add load/read function here  
+%     end    
+end 
+
+
+
+
+close all;
+end
--- Main.m
+++ Main.m
@@ -9,8 +9,26 @@
 
 %% Step 1: select the data from the appropriate directory.
 %
-% * The directory should contain the .dat and the .list files,
-% * it should have a descriptive name (e.g.
+% * The directory should contain the .data, the .list files, and the 
+% polarization schedule as an identifying key.  
+% * It should have a descriptive name (e.g.
 % reverse-date-of-scan_subject-ID_axial-or-coronal-orientation, if
 % possible), 
-% * 
+% * and also the bulk data should be stored in its own directory, on your CPIR-share
+% folder, as a centralized spot for sharing and storing the bulk data.
+%
+% 
+ImgOrientation = 'Coronal'; % just choose coronal or axial by commenting/uncommenting
+%ImgOrientation = 'Axial';
+MainInput.ScannerSoftware = '5.9.0';
+
+[filename, path] = uigetfile('*.*','Select .data file for the cartesian ventilation');
+MainInput.XeFullPath = [path,filename];
+MainInput.XeDataLocation = path(1:end-1);
+[~,~,xe_ext] = fileparts([path, filename]);
+MainInput.XeFileName = filename;
+MainInput.XeDataext = xe_ext;
+MainInput.ImgOrientation = ImgOrientation;
+
+Ventilation = Functions.LoadReadData_VentDataList(MainInput);
+orthosliceViewer(Ventilation.Image)
